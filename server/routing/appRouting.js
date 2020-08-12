230f15b1367de4b156c8ec54452aed5f77b64647
Engywook2005 <uig6fl*ht1ft>
2020-08-12 01:17:32 -0400

    Have a possibly workable JSON output for date, time, and load

--- server/routing/appRouting.js
+++ server/routing/appRouting.js
@@ -1,12 +1,47 @@
+const fs = require('fs');
+
 class AppRouting {
   constructor(app) {
     this.app = app;
+    this.loadProfile = [];
   }
 
   init() {
-    // @TODO not that sure we even need this.
-
     console.log(this.app);
+
+    this.prepLoadProfile();
+
+    this.app.get('/data/loadProfile', (request, response) => {
+      response.send(JSON.stringify(this.loadProfile));
+    });
+  }
+
+  /**
+   * Called once when server starts. In a real use case this would more likely be based on
+   * something more fluid than a static file, e.g. a database; so we would be getting that ready.
+   */
+  prepLoadProfile() {
+    // @TODO
+
+    // loadProfile created by running yarn run loadProfile.
+    fs.readFile('./sourceData/loadProfile.json', 'utf8', (err, data) => {
+      if (err) {
+        // @TODO handle
+      } else {
+        const loadProfile = JSON.parse(data);
+
+        loadProfile.forEach((loadDateHour) => {
+          const dateTimeArray = loadDateHour['Date/Time'].split('  ');
+          const newObj = {
+            date: dateTimeArray[0].replace(' ', ''),
+            hour: dateTimeArray[1],
+            load: loadDateHour['Electricity:Facility [kWh](Hourly)'],
+          };
+
+          this.loadProfile.push(newObj);
+        });
+      }
+    });
   }
 }
 
